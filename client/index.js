ae384292a858543041914ee50ceeccc4d746ca54
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-04-07 13:45:24 -0400

    updated url on index.js

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-fetch('https://pitching-rotation-api.onrender.com/api/pitching-rotation')
+fetch('https://pitching-rotation-frontend.onrender.com/api/pitching-rotation')
 	.then(response => response.json())
 	.then(data => console.log(data))
 	.catch(error => console.error(error))
