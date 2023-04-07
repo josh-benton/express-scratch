5f14af917bf33d30fecf55a747e475280ac9aa1a
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-04-07 14:53:18 -0400

    added patch and delete handlers on server.js

--- server.js
+++ server.js
@@ -43,6 +43,39 @@ app.post("/api/pitching-rotation", (req, res) => {
   });
 });
 
+app.patch("/api/pitching-rotation", (req, res) => {
+  const id = req.params.id;
+  const { player_name, throws, earned_run_average, hall_of_fame } = req.body;
+  const query = {
+    text: "UPDATE pitching_rotation SET player_name = $1, throws = $2, earned_run_average = $3, hall_of_fame = $4 WHERE id = $5",
+    values: [player_name, throws, earned_run_average, hall_of_fame, id],
+  };
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error updating record");
+    } else {
+      res.send("Record updated successfully");
+    }
+  });
+});
+
+app.delete("/api/pitching-rotation", (req, res) => {
+  const id = req.params.id;
+  const query = {
+    text: "DELETE FROM pitching_rotation WHERE id = $1",
+    values: [id],
+  };
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error deleting record");
+    } else {
+      res.send("Record deleted successfully");
+    }
+  });
+})
+
 // Start the server
 app.listen(port, () => {
   console.log("Server is listening on port 3000...");
